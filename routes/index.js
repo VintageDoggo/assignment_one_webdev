a2e3ae308569c3c7aa835b7b0a76a55157967cb4
JorgeCorichi <jorge.corichi@outlook.com>
2022-09-27 00:04:11 -0400

    Added bootstrap, nav bar partial and scripts partial

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'ABOUT ME' });
+});
+
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'CONTACT ME' });
+});
+
+/* GET My Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'MY PROJECTS' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'SERVICES' });
 });
 
 module.exports = router;
